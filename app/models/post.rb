922a6f4b6eeadf11cf81653464098f31b0e955be
Elise Phan <aliceconnic@gmail.com>
2019-02-11 22:52:35 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,13 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
-  validates :clickbait, acceptance: 
+  validate :clickbait, acceptance: 
   
   
+  def expiration_date_cannot_be_in_the_past
+    if expiration_date.present? && expiration_date < Date.today
+      errors.add(:expiration_date, "can't be in the past")
+    end
+  end
   
 end
