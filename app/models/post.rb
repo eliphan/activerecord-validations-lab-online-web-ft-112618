d241c136f955c45c7c5d5c02b5053ee626a57c98
Elise Phan <aliceconnic@gmail.com>
2019-02-11 22:53:41 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   validate :clickbait, acceptance: 
   
   
-  def expiration_date_cannot_be_in_the_past
+  def clickbait
     if expiration_date.present? && expiration_date < Date.today
       errors.add(:expiration_date, "can't be in the past")
     end
